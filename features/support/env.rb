4b66e5013562630132eff9452fbc6560df58f529
Jeni Tennison <jeni@theodi.org>
2015-10-25 13:50:49 +0000

    csv2json

---
+++ features/support/env.rb
@@ -0,0 +1,26 @@
+require 'coveralls'
+Coveralls.wear_merged!('test_frameworks')
+
+$:.unshift File.join( File.dirname(__FILE__), "..", "..", "lib")
+
+require 'rspec/expectations'
+require 'cucumber/rspec/doubles'
+require 'csvlint/csvw/csv2json'
+require 'pry'
+
+require 'spork'
+
+Spork.each_run do
+  require 'csvlint/csvw/csv2json'
+end
+
+class CustomWorld
+  def default_csv_options
+    return {
+    }
+  end
+end
+
+World do
+  CustomWorld.new
+end
